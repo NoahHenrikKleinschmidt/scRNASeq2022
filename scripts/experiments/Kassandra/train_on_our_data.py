c69fdc3dfe50e2fbad19644a582e1402d54bdadb
NoahHenrikKleinschmidt <noah.kleinschmidt@students.unibe.ch>
2022-08-19 08:54:15 +0200

    add kassandra (project currently dropped)

---
+++ scripts/experiments/Kassandra/train_on_our_data.py
@@ -0,0 +1,12 @@
+"""
+This script makes use of the self-packaged version of the Kassandra source code from Github. 
+It trains a model on the data that is available in the data/ folder.
+
+This script repeats the steps outlined in the notebook on Github.
+"""
+
+import Kassandra as ks
+import pandas as pd
+
+
+
